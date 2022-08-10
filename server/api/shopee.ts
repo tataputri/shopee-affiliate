b38352dc653538ebdfcfc470844210461a7216af
tataputri <doppelgangerr02@gmail.com>
2022-08-10 16:35:58 +0700

    2 files modified, 1 added

---
+++ server/api/shopee.ts
@@ -0,0 +1,9 @@
+import { sha256, sha224 } from 'js-sha256';
+export default defineEventHandler(async (event) => {
+  
+  const appId = '11317240145'
+  const secret = 'XFUQYZE2ZUPLZDWR2INUL7TYNSYRZ5VD'
+
+
+
+});

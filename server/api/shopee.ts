e072b27fdd314928e2abd1f44febb4e6a1b04430
tataputri <doppelgangerr02@gmail.com>
2022-08-10 16:39:42 +0700

    tes commit

--- server/api/shopee.ts
+++ server/api/shopee.ts
@@ -1,9 +1,12 @@
 import { sha256, sha224 } from 'js-sha256';
 export default defineEventHandler(async (event) => {
-  
-  const appId = '11317240145'
-  const secret = 'XFUQYZE2ZUPLZDWR2INUL7TYNSYRZ5VD'
+  const appId = '11317240145';
+  const secret = 'XFUQYZE2ZUPLZDWR2INUL7TYNSYRZ5VD';
+  const timeStamp = Math.floor(new Date().getTime() / 1000);
+  const jsonPayload = `{"query":"{\n  productOfferV2(keyword: \"%s\", sortType: %d,limit:%d,page:%d) {\n    nodes {\n      itemId\n      productName\n      imageUrl\n      offerLink\n      price\n      shopName\n      sales\n      commissionRate\n      periodStartTime\n      periodEndTime\n      \n    }\n  }\n}\n","variables":null,"operationName":null}`
+  const signature = sha256(appId + timeStamp + JSON.stringify(jsonPayload) + secret);
+  const authHeader = `SHA256 Credentials=${appId}, Timestamp=${timesStamp}, Signature=${signature}`;
 
-
+console.log(jsonPayload)
 
 });
